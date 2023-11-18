7bee22bc78e6c09a3af03621f27cca09fe65518e
Borbuilder <nikitamaximfcn@yandex.ru>
2023-11-18 15:41:02 +0300

    Написал класс TStack

--- mp2-lab3-stack -forgit/TStack.h
+++ mp2-lab3-stack -forgit/TStack.h
@@ -1,7 +1,160 @@
 #pragma once
+#include<iostream>
 
 template <class T>
 class TStack
 {
+private:
+	T* pMem;
+	int MaxSize;
+	int CurrentIndex;
+
+public:
+	
+	TStack(int ms);   // Конструктор по умолчанию
+	TStack(const TStack& StackObject);  // Конструктор копирования
+
+	bool operator ==(const TStack& StackObject);  // Оператор сравнения
+	bool operator !=(const TStack& StackObject);  // Оператор сравнения 2
+	TStack& operator = (const TStack& StackObject);  // Оператор присваивания
+
+	bool empty(); // Метод проверки стека на пустоту
+	bool full(); // Метод проверки стека на полноту
+	int ElementsNumber(); // Количесвто элементов в стеке
+	void output(); // Метод вывода стека на экран 
+	void Push(const T element); // Метод добавления элемента в стек
+	T Pop(); // Метод получения вершины стека (c её перемещением)
+	T Top(); // Метод получения значения вершины стека
 };
+
+
+//Реализация методов класса TStack
+
+template <class T> //Конструктор по умолчанию
+TStack<T>::TStack(int ms) 
+{
+	MaxSize = ms;
+	pMem = new T[MaxSize];
+	CurrentIndex = -1;
+}
+
+template <class T> // Конструктор копирования
+TStack<T>::TStack(const TStack& StackObject)
+{
+	MaxSize = StackObject.MaxSize;
+	delete[] pMem;
+	pMem = new T[MaxSize];
+	CurrentIndex = StackObject.CurrentIndex;
+	std::copy(StackObject.pMem, StackObject.pMem + CurrentIndex, pMem);
+}
+
+template <class T> //Оператор сравнения
+bool TStack<T>:: operator ==(const TStack& StackObject)
+{
+	if ((CurrentIndex) != (StackObject.CurrentIndex))
+	{
+		return false;
+	}
+	for (int i = 0; i < CurrentIndex; i++)
+	{
+		if (pMem[i] != StackObject.pMem[i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+template <class T> //Оператор сравнения 2
+bool TStack<T>:: operator !=(const TStack& StackObject)
+{
+	return !(*this == StackObject);
+}
+
+template <class T> //Оператор присваивания
+TStack<T>& TStack<T>::operator = (const TStack& StackObject)
+{
+	if (*this != StackObject)
+	{
+		delete[] pMem;
+		MaxSize = StackObject.MaxSize;
+		CurrentIndex = StackObject.CurrentIndex;
+		pMem = new T[MaxSize];
+		std::copy(StackObject.pMem, StackObject.pMem + CurrentIndex, pMem);
+	}
+	return *this;
+}
+
+template <class T> //Проверка на пустоту
+bool TStack<T>::empty()
+{
+	if (CurrentIndex == -1)
+	{
+		return true;
+	}
+	return false;
+}
+
+template <class T> //Проверка на полноту
+bool TStack<T>::full()
+{
+	if (CurrentIndex == MaxSize - 1)
+	{
+		return true;
+	}
+	return false;
+}
+
+template <class T> //Количество элементов
+int TStack<T>::ElementsNumber()
+{
+	return CurrentIndex + 1;
+}
+
+template<class T>//Вывод
+void TStack<T>::output()
+{
+	if (CurrentIndex == -1)
+	{
+		std::cout << "Stack is empty" << std::endl;
+	}
+	std::cout << "( ";
+	for (int i = 0; i < CurrentIndex; i++)
+	{
+		std::cout << pMem[i] << " ";
+	}
+	std::cout << ")" << std::endl;
+}
+
+template <class T> //Добавление элемента
+void TStack<T>::Push(const T element)
+{
+	if (CurrentIndex + 1 >= MaxSize)
+	{
+		throw "Stack is overfloated";
+	}
+	pMem[CurrentIndex + 1] = element;
+	CurrentIndex++;
+}
+
+template <class T> //Получение вершины
+T TStack<T>::Pop()
+{
+	if (CurrentIndex == -1)
+	{
+		throw "Stack is empty";
+	}
+	CurrentIndex--;
+	return pMem[CurrentIndex + 1];
+}
+
+template <class T> //Значение вершины
+T TStack<T>::Top()
+{
+	if (CurrentIndex == -1)
+	{
+		throw "Stack is empty";
+	}
+	return pMem[CurrentIndex + 1];
+}
 
--- mp2-lab3-stack -forgit/mp2-lab3-stack-main.cpp
+++ mp2-lab3-stack -forgit/mp2-lab3-stack-main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-void main()
-{
-	std::cout << "Hello";
+int sravn(const int *arrr,const int *arrr2) {
+	if (arrr == arrr2) {
+		return 1;
+	}
+	return 0;
 }
+
+int main()
+{
+	int size = 3; int size2 = 5;
+	int* arr; int* arr2;
+	arr = new int[size];
+	arr2 = new int[size2];
+	std::cout << sravn(arr, arr);
+
+};
